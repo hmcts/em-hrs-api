7092f21b194d9406831ce504a4f45b168b701108
rajkatla-hmcts <72351979+rajkatla-hmcts@users.noreply.github.com>
2021-04-30 13:08:52 +0100

    refactored and fixed functional tests junit version issue

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/functional/config/HrsAzureClient.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/functional/config/HrsAzureClient.java
@@ -1,10 +1,15 @@
-package uk.gov.hmcts.reform.em.hrs.config;
+package uk.gov.hmcts.reform.em.hrs.functional.config;
 
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+<<<<<<<< HEAD:src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/functional/config/HrsAzureClient.java
+
+import static org.slf4j.LoggerFactory.getLogger;
+========
+>>>>>>>> refactored and fixed functional tests junit version issue:src/aat/java/uk/gov/hmcts/reform/em/hrs/config/HrsAzureClient.java
 
 @Component
 public class HrsAzureClient {
@@ -21,7 +26,7 @@ public class HrsAzureClient {
     @Value("${azure.storage.cvp.blob-container-reference}")
     private String cvpContainer;
 
-    @Bean
+    @Bean(name = "hrsBlobContainerClient")
     public BlobContainerClient hrsBlobContainerClient() {
         return new BlobContainerClientBuilder()
             .connectionString(hrsConnectionString)
@@ -29,7 +34,7 @@ public class HrsAzureClient {
             .buildClient();
     }
 
-    @Bean
+    @Bean(name = "cvpBlobContainerClient")
     public BlobContainerClient cvpBlobContainerClient() {
         return new BlobContainerClientBuilder()
             .connectionString(cvpConnectionString)
