bde21841ef6bbf725bd1360daf7b1fe7485aee78
michaeljacob <Michael.Jacob@HMCTS.NET>
2022-06-22 12:04:17 +0100

    Add cftlib

--- src/main/java/uk/gov/hmcts/reform/em/hrs/config/AzureStorageConfig.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/config/AzureStorageConfig.java
@@ -82,8 +82,15 @@ public class AzureStorageConfig {
 
         }
 
+        BlobContainerClient blobContainerClient = b.buildClient();
+        final boolean containerExists = Optional.ofNullable(blobContainerClient.exists())
+            .orElse(false);
 
-        return b.buildClient();
+        if (!containerExists) {
+            LOGGER.info("Creating container {} in HRS Storage", cvpContainer);
+            blobContainerClient.create();
+        }
+        return blobContainerClient;
     }
 
 }
