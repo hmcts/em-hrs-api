36b22fdcd28a17565305f1a7060d43d22b3cfc26
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2022-06-22 10:05:04 +0100

    Changes Test package

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/ShareScenarios.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/ShareScenarios.java
@@ -1,9 +1,8 @@
 package uk.gov.hmcts.reform.em.hrs;
 
-
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
