53d8173c8442f74d9bed949a17948d40f182f35e
Jefex7 <duartepireslopes@gmail.com>
2023-04-18 17:06:14 +0100

    Created Welcome Controller and test

--- src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.em.hrs.smoke;
 
 import io.restassured.RestAssured;
+import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -16,8 +20,29 @@ import static org.hamcrest.Matchers.equalTo;
 @WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
 
+    private static final String MESSAGE = "Welcome to DM Store API!";
+
     @Value("${test.url}")
     private String testUrl;
+
+    @Test
+    public void testHealthWelcomeEndpoint() {
+        SerenityRest.useRelaxedHTTPSValidation();
+
+        Map responseMap =
+            SerenityRest
+                .given()
+                .baseUri(testUrl)
+                .get("/")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(Map.class);
+
+        Assert.assertEquals(1, responseMap.size());
+        Assert.assertEquals(MESSAGE, responseMap.get("message"));
+    }
 
     @Test
     public void testHealthEndpoint() {
---
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/controller/WelcomeControllerTest.java
@@ -0,0 +1,40 @@
+package uk.gov.hmcts.reform.em.hrs.controller;
+
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class WelcomeControllerTest {
+
+    private final WelcomeController welcomeController = new WelcomeController();
+
+    @Test
+    public void testEndPointResponseCode() {
+        ResponseEntity<Map<String, String>> responseEntity = welcomeController.welcome();
+
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void testEndpointResponseMessage() {
+        ResponseEntity<Map<String, String>> responseEntity = welcomeController.welcome();
+
+        Map<String, String> expectedResponse = new HashMap<>();
+        expectedResponse.put("message", "Welcome to the HRS API!");
+
+        String cacheHeader = responseEntity.getHeaders().getCacheControl();
+
+        assertNotNull(responseEntity);
+        assertEquals("no-cache", cacheHeader);
+        assertEquals(expectedResponse, responseEntity.getBody());
+    }
+
+
+}
