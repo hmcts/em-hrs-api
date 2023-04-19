c1596252e5cc52f903988c9de37e687c322b817e
Jefex7 <duartepireslopes@gmail.com>
2023-04-19 12:29:35 +0100

    Removed Welcome endpoint smokeTest

--- src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
@@ -2,17 +2,13 @@ package uk.gov.hmcts.reform.em.hrs.smoke;
 
 import io.restassured.RestAssured;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.em.hrs.smoke.config.SmokeTestContextConfiguration;
-
-import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 
@@ -25,25 +21,6 @@ public class SmokeTest {
 
     @Value("${test.url}")
     private String testUrl;
-
-    @Test
-    public void testHealthWelcomeEndpoint() {
-        SerenityRest.useRelaxedHTTPSValidation();
-
-        Map responseMap =
-            SerenityRest
-                .given()
-                .baseUri(testUrl)
-                .get("/")
-                .then()
-                .statusCode(200)
-                .extract()
-                .body()
-                .as(Map.class);
-
-        Assert.assertEquals(1, responseMap.size());
-        Assert.assertEquals(MESSAGE, responseMap.get("message"));
-    }
 
     @Test
     public void testHealthEndpoint() {
