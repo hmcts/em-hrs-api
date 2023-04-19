fe72707b18d58210f7786f1c6404d48fefdd5bbe
Jefex7 <duartepireslopes@gmail.com>
2023-04-19 11:50:50 +0100

    Added SmokeTestContextConfiguration file

--- src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
@@ -1,26 +1,27 @@
 package uk.gov.hmcts.reform.em.hrs.smoke;
 
 import io.restassured.RestAssured;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.em.hrs.smoke.config.SmokeTestContextConfiguration;
 
 import java.util.Map;
 
 import static org.hamcrest.Matchers.equalTo;
 
-@ExtendWith(SpringExtension.class)
-@TestPropertySource(value = "classpath:application.yml")
+@SpringBootTest(classes = SmokeTestContextConfiguration.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
 
-    private static final String MESSAGE = "Welcome to DM Store API!";
+    private static final String MESSAGE = "Welcome to Welcome to the HRS API!";
 
     @Value("${test.url}")
     private String testUrl;
