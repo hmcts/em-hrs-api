bc35d86f9628c891d0f6a837c4602d22f70d0371
Jefex7 <duartepireslopes@gmail.com>
2023-04-19 13:22:20 +0100

    Reverting smokeTest

--- src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
@@ -1,61 +1,35 @@
 package uk.gov.hmcts.reform.em.hrs.smoke;
 
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.serenitybdd.rest.SerenityRest;
+import io.restassured.RestAssured;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import uk.gov.hmcts.reform.em.hrs.smoke.config.SmokeTestContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Map;
+import static org.hamcrest.Matchers.equalTo;
 
-
-@SpringBootTest(classes = SmokeTestContextConfiguration.class)
-@RunWith(SpringIntegrationSerenityRunner.class)
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(value = "classpath:application.yml")
 @WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
-
-    private static final String MESSAGE = "Welcome to Welcome to the HRS API!";
 
     @Value("${test.url}")
     private String testUrl;
 
     @Test
-    public void testHealthWelcomeEndpoint() {
-        SerenityRest.useRelaxedHTTPSValidation();
-
-        Map responseMap =
-            SerenityRest
-                .given()
-                .baseUri(testUrl)
-                .get("/")
-                .then()
-                .statusCode(200)
-                .extract()
-                .body()
-                .as(Map.class);
-
-        Assert.assertEquals(1, responseMap.size());
-        Assert.assertEquals(MESSAGE, responseMap.get("message"));
-    }
-
-    @Test
     public void testHealthEndpoint() {
-        SerenityRest.useRelaxedHTTPSValidation();
 
-        String response = SerenityRest
+        RestAssured
             .given()
+            .relaxedHTTPSValidation()
             .baseUri(testUrl)
+            .when()
             .get("/health")
             .then()
             .statusCode(200)
-            .extract()
-            .body().jsonPath().getString("status");
-
-        Assert.assertEquals("UP", response);
+            .body("status", equalTo("UP"));
     }
 }
