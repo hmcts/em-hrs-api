6f1c9857c5f001837d0a9c694c2e181b0f114472
Jefex7 <duartepireslopes@gmail.com>
2023-04-19 13:07:05 +0100

    Updated testHealthEndpoint method

--- src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/em/hrs/smoke/SmokeTest.java
@@ -1,16 +1,18 @@
 package uk.gov.hmcts.reform.em.hrs.smoke;
 
-import io.restassured.RestAssured;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.WithTag;
 import net.thucydides.core.annotations.WithTags;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.gov.hmcts.reform.em.hrs.smoke.config.SmokeTestContextConfiguration;
 
-import static org.hamcrest.Matchers.equalTo;
+import java.util.Map;
+
 
 @SpringBootTest(classes = SmokeTestContextConfiguration.class)
 @RunWith(SpringIntegrationSerenityRunner.class)
@@ -23,17 +25,37 @@ public class SmokeTest {
     private String testUrl;
 
     @Test
-    public void testHealthEndpoint() {
+    public void testHealthWelcomeEndpoint() {
+        SerenityRest.useRelaxedHTTPSValidation();
 
-        RestAssured
+        Map responseMap =
+            SerenityRest
+                .given()
+                .baseUri(testUrl)
+                .get("/")
+                .then()
+                .statusCode(200)
+                .extract()
+                .body()
+                .as(Map.class);
+
+        Assert.assertEquals(1, responseMap.size());
+        Assert.assertEquals(MESSAGE, responseMap.get("message"));
+    }
+
+    @Test
+    public void testHealthEndpoint() {
+        SerenityRest.useRelaxedHTTPSValidation();
+
+        String response = SerenityRest
             .given()
-            .relaxedHTTPSValidation()
             .baseUri(testUrl)
-            .when()
             .get("/health")
             .then()
             .statusCode(200)
-            .body("status", equalTo("UP"));
-    }
+            .extract()
+            .body().jsonPath().getString("status");
 
+        Assert.assertEquals("UP", response);
+    }
 }
