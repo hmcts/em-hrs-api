957c2de4318eb06af437f7189176b4269f83da5f
mdayican <mdayican@gmail.com>
2023-07-05 09:19:49 +0100

    EnableGlobalMethodSecurity

--- src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -28,7 +28,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @Profile({"!integration-web-test"})
 public class SecurityConfiguration {
 
