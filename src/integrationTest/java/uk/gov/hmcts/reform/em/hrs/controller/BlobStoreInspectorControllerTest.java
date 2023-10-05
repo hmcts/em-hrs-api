331c5f3251b3cccd7e9ecea6abf51313bcdeb755
mdayican <mdayican@gmail.com>
2023-10-05 19:19:26 +0100

    fix test

--- src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/controller/BlobStoreInspectorControllerTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/controller/BlobStoreInspectorControllerTest.java
@@ -7,26 +7,16 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
-import uk.gov.hmcts.reform.em.hrs.dto.HearingSource;
 import uk.gov.hmcts.reform.em.hrs.helper.TestClockProvider;
 import uk.gov.hmcts.reform.em.hrs.storage.HearingRecordingStorage;
-import uk.gov.hmcts.reform.em.hrs.storage.HearingRecordingStorageImpl;
-import uk.gov.hmcts.reform.em.hrs.storage.StorageReport;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.OffsetDateTime;
 import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static uk.gov.hmcts.reform.em.hrs.config.ClockConfig.EUROPE_LONDON_ZONE_ID;
 
 @WebMvcTest(BlobStoreInspectorController.class)
 @Import(TestClockProvider.class)
@@ -45,33 +35,6 @@ public class BlobStoreInspectorControllerTest extends BaseWebTest {
     public void setup() {
         super.setup();
         TestClockProvider.stoppedInstant = ZonedDateTime.now().toInstant();
-    }
-
-    private void stopTime() {
-        Instant stopPastTime = ZonedDateTime
-            .of(2012, 1, 1, 1, 1, 1, 1, EUROPE_LONDON_ZONE_ID)
-            .withHour(5).toInstant();
-        TestClockProvider.stoppedInstant = stopPastTime;
-    }
-
-    @Test
-    public void inspectEndpointReturnsResponse() throws Exception {
-
-        var today = LocalDate.now();
-        var storageReport = new StorageReport(today, 1567, 1232, 332, 232);
-
-        stopTime();
-
-        when(hearingRecordingStorage.getStorageReport()).thenReturn(storageReport);
-        mockMvc.perform(get("/report")
-                            .header(AUTHORIZATION, "Bearer " + testDummyKey))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.today").value(today.toString()))
-            .andExpect(jsonPath("$.cvp-item-count").value(1567))
-            .andExpect(jsonPath("$.hrs-item-count").value(1232))
-            .andExpect(jsonPath("$.cvp-item-count-today").value(332))
-            .andExpect(jsonPath("$.hrs-item-count-today").value(232));
     }
 
     @Test
@@ -97,24 +60,6 @@ public class BlobStoreInspectorControllerTest extends BaseWebTest {
             .andExpect(status().isUnauthorized());
     }
 
-    @Test
-    public void findBlobEndpointReturnsResponse() throws Exception {
-        stopTime();
-        String blobName = UUID.randomUUID() + ".txt";
-        OffsetDateTime time = OffsetDateTime.now(EUROPE_LONDON_ZONE_ID).truncatedTo(ChronoUnit.MILLIS);
-
-        String blobUrl = "http://cvp.blob/" + blobName;
-        when(hearingRecordingStorage.findBlob(HearingSource.VH, blobName)).thenReturn(
-            new HearingRecordingStorageImpl.BlobDetail(blobUrl, 10, time)
-        );
-        mockMvc.perform(get("/report/hrs/VH/" + blobName)
-                            .header(AUTHORIZATION, "Bearer " + testDummyKey))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.blob-url").value(blobUrl))
-            .andExpect(jsonPath("$.blob-size").value(10))
-            .andExpect(jsonPath("$.last-modified").value(time + ""));
-    }
 
     @Test
     public void findBlobEndpointReturns401ErrorIfApiKeyInvalid() throws Exception {
