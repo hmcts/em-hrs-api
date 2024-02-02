bfda14df00d00b60c806104fb8a15613b8a3e776
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-02-02 14:50:12 +0000

    update deprecated config

--- src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
@@ -19,7 +20,7 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationFilter;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 
@@ -62,15 +63,15 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
-            .formLogin(login -> login.disable())
-            .logout(logout -> logout.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
             .addFilterBefore(serviceAuthFilter, BearerTokenAuthenticationFilter.class)
             .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeRequests()
-            .requestMatchers(HttpMethod.GET, "/hearing-recordings/**").authenticated()
-            .requestMatchers(HttpMethod.POST, "/sharees").authenticated()
-            .and()
+            .authorizeHttpRequests(authorizedUrl ->
+                authorizedUrl.requestMatchers(HttpMethod.GET, "/hearing-recordings/**").authenticated())
+            .authorizeHttpRequests(authorizedUrl ->
+                authorizedUrl.requestMatchers(HttpMethod.POST, "/sharees").authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
             .oauth2Client(withDefaults());
         return http.build();
