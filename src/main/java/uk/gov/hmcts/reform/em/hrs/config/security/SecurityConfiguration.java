702aec495b1544c1fe529a88dba5d3e9370b7675
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-02-15 09:31:07 +0000

    bring back filter

--- src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
@@ -7,12 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -20,9 +22,11 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 
@@ -56,7 +60,8 @@ public class SecurityConfiguration {
             "/status/health",
             "/loggers/**",
             "/report/**",
-            "/");
+            "/"
+        );
     }
 
     @Bean
@@ -79,25 +84,28 @@ public class SecurityConfiguration {
         return http.build();
     }
 
-    //    @Bean
-    //    @Order(2)
-    //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    //
-    //        http.csrf(AbstractHttpConfigurer::disable)
-    //            .formLogin(AbstractHttpConfigurer::disable)
-    //            .logout(AbstractHttpConfigurer::disable)
-    //            .addFilterBefore(serviceAuthFilter, BearerTokenAuthenticationFilter.class)
-    //            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    //            .authorizeHttpRequests(matcherRegistry ->
-    //                matcherRegistry.requestMatchers(HttpMethod.GET, "/hearing-recordings/**").authenticated())
-    //            .authorizeHttpRequests(matcherRegistry ->
-    //                matcherRegistry.requestMatchers(HttpMethod.POST, "/sharees").authenticated())
-    //            .authorizeHttpRequests(matcherRegistry ->
-    //                matcherRegistry.requestMatchers("/error").authenticated())
-    //            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
-    //            .oauth2Client(withDefaults());
-    //        return http.build();
-    //    }
+    @Bean
+    @Order(2)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .addFilterBefore(emServiceAuthFilter, BearerTokenAuthenticationFilter.class)
+            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(matcherRegistry ->
+                                       matcherRegistry.requestMatchers(
+                                           HttpMethod.GET,
+                                           "/hearing-recordings/**"
+                                       ).authenticated())
+            .authorizeHttpRequests(matcherRegistry ->
+                                       matcherRegistry.requestMatchers(HttpMethod.POST, "/sharees").authenticated())
+            .authorizeHttpRequests(matcherRegistry ->
+                                       matcherRegistry.requestMatchers("/error").authenticated())
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+            .oauth2Client(withDefaults());
+        return http.build();
+    }
 
     @Bean
     JwtDecoder jwtDecoder() {
