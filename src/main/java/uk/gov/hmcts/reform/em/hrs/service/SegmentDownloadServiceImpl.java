f93756be1a9760fc92530b6af95e7593a39895f0
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2024-03-02 17:45:06 +0000

    Updated log level

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/SegmentDownloadServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/SegmentDownloadServiceImpl.java
@@ -120,7 +120,8 @@ public class SegmentDownloadServiceImpl implements SegmentDownloadService {
             List<HearingRecordingSharee> hearingRecordingSharees =
                 shareesRepository.findByShareeEmailIgnoreCase(userEmail);
             LOGGER.info("User  {} is trying to access the recordingId  {} with segment Name {}",
-                         userEmail, recordingId, segmentName); // Needs to be changed to DEBUG level after validation on Prod.
+                         userEmail, recordingId, segmentName);
+            // Needs to be changed to DEBUG level after validation on Prod.
             if (!isEmpty(hearingRecordingSharees)) {
                 LOGGER.debug("User  {} has shared recordings", userEmail);
                 Optional<HearingRecordingSharee> recordingSharee = hearingRecordingSharees.stream()
