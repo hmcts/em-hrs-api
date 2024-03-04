44313703def1fc1803d43a8b9b8d3984f96ce60e
mustafa <mustafa.dayican1@hmcts.net>
2024-03-04 14:39:48 +0000

    Update SecurityConfiguration.java

--- src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/config/security/SecurityConfiguration.java
@@ -76,7 +76,8 @@ public class SecurityConfiguration {
                          httpSecurityHeadersConfigurer.cacheControl(HeadersConfigurer.CacheControlConfig::disable));
 
         http.securityMatchers(requestMatcherConfigurer ->
-                                  requestMatcherConfigurer.anyRequest())
+                                     requestMatcherConfigurer
+                                      .requestMatchers("/hearing-recordings", "/segments", "/folders/*"))
             .addFilterBefore(emServiceAuthFilter, AnonymousAuthenticationFilter.class)
             .sessionManagement(httpSecuritySessionManagementConfigurer ->
                                    httpSecuritySessionManagementConfigurer.sessionCreationPolicy(STATELESS))
