58dfc225a9ab726eb4fecef537168f3d57f3a73e
mdayican <mdayican@gmail.com>
2024-04-13 10:16:36 +0100

    EM-5865 delete VH sharee

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/DeleteVhRecordingTask.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/DeleteVhRecordingTask.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.em.hrs.repository.HearingRecordingRepository;
+import uk.gov.hmcts.reform.em.hrs.repository.ShareesRepository;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,9 +21,14 @@ public class DeleteVhRecordingTask {
     private static final Logger logger = getLogger(DeleteVhRecordingTask.class);
 
     private final HearingRecordingRepository hearingRecordingRepository;
+    private final ShareesRepository shareesRepository;
 
-    public DeleteVhRecordingTask(HearingRecordingRepository hearingRecordingRepository) {
+    public DeleteVhRecordingTask(
+        HearingRecordingRepository hearingRecordingRepository,
+        ShareesRepository shareesRepository
+    ) {
         this.hearingRecordingRepository = hearingRecordingRepository;
+        this.shareesRepository = shareesRepository;
     }
 
     @Scheduled(cron = "${scheduling.task.delete-vh-recordings.cron}", zone = "Europe/London")
@@ -33,10 +39,11 @@ public class DeleteVhRecordingTask {
         for (var id : recordsToDelete) {
             try {
                 logger.info("Deleting id {} ", id);
+                shareesRepository.deleteByHearingRecordingId(id);
                 hearingRecordingRepository.deleteById(id);
                 logger.info("Deleted id {} ", id);
             } catch (Exception ex) {
-                logger.error("deleting failed", ex);
+                logger.info("deleting failed", ex);
             }
         }
         logger.info("Finished {} job,record count {}", TASK_NAME, recordsToDelete);
--- src/test/java/uk/gov/hmcts/reform/em/hrs/service/DeleteVhRecordingTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/service/DeleteVhRecordingTaskTest.java
@@ -4,28 +4,38 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.em.hrs.repository.HearingRecordingRepository;
+import uk.gov.hmcts.reform.em.hrs.repository.ShareesRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DeleteVhRecordingTaskTest {
 
     @Mock
     private HearingRecordingRepository hearingRecordingRepository;
-
+    @Mock
+    private ShareesRepository shareesRepository;
     @InjectMocks
     private DeleteVhRecordingTask deleteVhRecordingTask;
 
     @Test
     void run_ShouldDeleteVhRecordings_WhenCalled() {
+        var uuid = UUID.randomUUID();
+        when(hearingRecordingRepository.listVhRecordingsToDelete()).thenReturn(List.of(uuid));
         deleteVhRecordingTask.run();
         verify(hearingRecordingRepository, times(1))
             .listVhRecordingsToDelete();
-        verify(hearingRecordingRepository, times(0))
-            .deleteById(any());
+        verify(shareesRepository, times(1)).deleteByHearingRecordingId(uuid);
+        verify(hearingRecordingRepository, times(1))
+            .deleteById(uuid);
     }
 }
