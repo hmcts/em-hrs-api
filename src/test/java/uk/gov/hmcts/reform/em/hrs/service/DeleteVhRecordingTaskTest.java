a4a8445f6afc719010d106e585619a3093c309f4
mdayican <mdayican@gmail.com>
2024-04-16 08:12:28 +0100

    EM-5865 delete audit data

--- src/test/java/uk/gov/hmcts/reform/em/hrs/service/DeleteVhRecordingTaskTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/hrs/service/DeleteVhRecordingTaskTest.java
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.em.hrs.domain.HearingRecording;
 import uk.gov.hmcts.reform.em.hrs.repository.HearingRecordingRepository;
+import uk.gov.hmcts.reform.em.hrs.repository.ShareesAuditEntryRepository;
 import uk.gov.hmcts.reform.em.hrs.repository.ShareesRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.Mockito.times;
@@ -23,16 +26,24 @@ class DeleteVhRecordingTaskTest {
     @Mock
     private ShareesRepository shareesRepository;
 
+    @Mock
+    private ShareesAuditEntryRepository hearingRecordingShareeAuditEntryRepository;
+
     @InjectMocks
     private DeleteVhRecordingTask deleteVhRecordingTask;
 
     @Test
     void run_ShouldDeleteVhRecordings_WhenCalled() {
         var uuid = UUID.randomUUID();
+        var hearingRecording = new HearingRecording();
+        hearingRecording.setCcdCaseId(1234567890L);
         when(hearingRecordingRepository.listVhRecordingsToDelete()).thenReturn(List.of(uuid));
+        when(hearingRecordingRepository.findById(uuid)).thenReturn(Optional.of(hearingRecording));
         deleteVhRecordingTask.run();
         verify(hearingRecordingRepository, times(1))
             .listVhRecordingsToDelete();
+        verify(hearingRecordingShareeAuditEntryRepository, times(1))
+            .deleteByCaseRef(1234567890L);
         verify(shareesRepository, times(1)).deleteByHearingRecordingId(uuid);
         verify(hearingRecordingRepository, times(1))
             .deleteById(uuid);
