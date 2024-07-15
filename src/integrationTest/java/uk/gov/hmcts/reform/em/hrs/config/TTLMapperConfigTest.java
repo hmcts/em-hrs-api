fbd50c2d8d343d4381dd068d63742271b118e493
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-07-15 12:10:01 +0100

    add configuration

--- src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/config/TTLMapperConfigTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/config/TTLMapperConfigTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.TestPropertySources;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.em.hrs.componenttests.config.TestAzureStorageConfig;
+import uk.gov.hmcts.reform.em.hrs.helper.AzureIntegrationTestOperations;
+import uk.gov.hmcts.reform.em.hrs.storage.BlobstoreClientImpl;
 
 import java.time.Period;
 import java.util.Map;
@@ -14,14 +18,18 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {
+    TestAzureStorageConfig.class,
+    BlobstoreClientImpl.class,
+    AzureIntegrationTestOperations.class
+})
 @EnableConfigurationProperties(value = TTLMapperConfig.class)
 @TestPropertySources({
     @TestPropertySource(value = "classpath:ttl_jurisdiction_map.json",
         factory  = TTLPropertySourceFactory.class),
     @TestPropertySource(value = "classpath:ttl_service_map.json",
         factory  = TTLPropertySourceFactory.class),
-}
-)
+})
 class TTLMapperConfigTest {
 
     @Autowired
