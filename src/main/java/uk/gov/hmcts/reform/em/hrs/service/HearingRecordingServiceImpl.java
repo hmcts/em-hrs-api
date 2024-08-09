6cd0254b7bf584cc1a66a30f677df7277e3a3fe8
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-08-09 16:22:56 +0100

    create delete endpoint

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/HearingRecordingServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/HearingRecordingServiceImpl.java
@@ -1,11 +1,13 @@
 package uk.gov.hmcts.reform.em.hrs.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.em.hrs.repository.HearingRecordingRepository;
 
 import java.util.Collection;
 
 @Service
+@Transactional
 public class HearingRecordingServiceImpl implements HearingRecordingService {
     private final HearingRecordingRepository hearingRecordingRepository;
 
