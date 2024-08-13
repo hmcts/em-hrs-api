3224355c3f5963123f38911439272f1e826c1a29
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-08-13 17:07:02 +0100

    add relationship

--- src/main/java/uk/gov/hmcts/reform/em/hrs/domain/HearingRecording.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/domain/HearingRecording.java
@@ -81,6 +81,9 @@ public class HearingRecording {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "hearingRecording")
     private Set<HearingRecordingSegment> segments;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "hearingRecording")
+    private Set<HearingRecordingSharee> sharees;
+
     public HearingRecording(UUID id, String createdBy, String createdByService, String lastModifiedBy,
                             String lastModifiedByService,
                             LocalDateTime modifiedOn, LocalDateTime createdOn,
@@ -90,7 +93,8 @@ public class HearingRecording {
                             String recordingRef, String caseRef, String hearingLocationCode,
                             String hearingRoomRef, String hearingSource,
                             String jurisdictionCode, String serviceCode, Long ccdCaseId,
-                            Set<HearingRecordingSegment> segments) {
+                            Set<HearingRecordingSegment> segments,
+                            Set<HearingRecordingSharee> sharees) {
         setId(id);
         setCreatedBy(createdBy);
         setCreatedByService(createdByService);
@@ -116,6 +120,7 @@ public class HearingRecording {
         setCcdCaseId(ccdCaseId);
 
         setSegments(segments);
+        setSharees(sharees);
     }
 
     public HearingRecording() {
