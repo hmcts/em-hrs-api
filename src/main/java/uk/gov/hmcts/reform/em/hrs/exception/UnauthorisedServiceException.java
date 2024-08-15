486f3e124261e3778d341e6431e0e4040385e5a6
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-08-15 15:52:40 +0100

    add interceptor

---
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/exception/UnauthorisedServiceException.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.reform.em.hrs.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorisedServiceException extends RuntimeException {
+    public UnauthorisedServiceException(String message) {
+        super(message);
+    }
+}
