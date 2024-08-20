2740c5ea655cb67cdf9bb62d136b195054a788be
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-08-20 09:48:52 +0100

    Revert "disable tests"
    This reverts commit 0d37e03072cc0fb38b1edeadffa7ad92f2144e29.

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/SecurityScenarios.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/SecurityScenarios.java
@@ -2,12 +2,10 @@ package uk.gov.hmcts.reform.em.hrs;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import net.serenitybdd.rest.SerenityRest;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@Ignore
 public class SecurityScenarios extends BaseTest {
 
     @Test
