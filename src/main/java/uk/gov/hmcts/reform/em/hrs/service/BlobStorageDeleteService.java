bbc7f083ceb734833caecc040c3d4ffe619b584f
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-09-26 17:08:45 +0100

    unused imports

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/BlobStorageDeleteService.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/BlobStorageDeleteService.java
@@ -10,8 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.em.hrs.dto.HearingSource;
 
 import static java.lang.Boolean.TRUE;
