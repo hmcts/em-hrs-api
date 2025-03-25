9ab1b4b839e9912eee908432f4df93f63ced25b3
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-03-25 09:16:32 +0000

    implement runnable

--- src/main/java/uk/gov/hmcts/reform/em/hrs/job/UpdateTtlJob.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/job/UpdateTtlJob.java
@@ -1,11 +1,8 @@
 package uk.gov.hmcts.reform.em.hrs.job;
 
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.domain.Limit;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.em.hrs.domain.HearingRecording;
 import uk.gov.hmcts.reform.em.hrs.repository.HearingRecordingRepository;
@@ -20,8 +17,7 @@ import java.util.concurrent.Executors;
 import static org.slf4j.LoggerFactory.getLogger;
 
 @Component
-@ConditionalOnProperty(value = "scheduling.task.update-ttl.enabled")
-public class UpdateTtlJob {
+public class UpdateTtlJob implements Runnable{
 
     private static final String TASK_NAME = "update-ttl";
     private static final Logger logger = getLogger(UpdateTtlJob.class);
@@ -44,8 +40,6 @@ public class UpdateTtlJob {
         this.ccdDataStoreApiClient = ccdDataStoreApiClient;
     }
 
-    @Scheduled(cron = "${scheduling.task.update-ttl.cron}", zone = "Europe/London")
-    @SchedulerLock(name = TASK_NAME)
     public void run() {
         logger.info("Started {} job", TASK_NAME);
 
