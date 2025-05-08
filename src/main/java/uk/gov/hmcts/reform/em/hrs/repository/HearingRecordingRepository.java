4c66745b014533340d2202fa8fa203ce1f67e5cb
lars <lars.lunde@hmcts.net>
2025-05-08 08:44:40 +0100

    checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/em/hrs/repository/HearingRecordingRepository.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/repository/HearingRecordingRepository.java
@@ -1,14 +1,12 @@
 package uk.gov.hmcts.reform.em.hrs.repository;
 
 import jakarta.transaction.Transactional;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.em.hrs.domain.HearingRecording;
-import uk.gov.hmcts.reform.em.hrs.dto.HearingRecordingTtlMigrationDTO;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
