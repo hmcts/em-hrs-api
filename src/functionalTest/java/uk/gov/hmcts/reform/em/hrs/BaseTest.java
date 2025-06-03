b18ebd8b27e7f78bd64b71cbeee96d782d9d341a
mdayican <mdayican@gmail.com>
2025-06-03 08:04:11 +0100

    upload ccd defs

--- src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/BaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/em/hrs/BaseTest.java
@@ -41,6 +41,7 @@ import uk.gov.hmcts.reform.em.test.retry.RetryExtension;
 import uk.gov.hmcts.reform.em.test.s2s.S2sHelper;
 import uk.gov.hmcts.reform.idam.client.IdamClient;
 
+import java.io.IOException;
 import java.security.SecureRandom;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
@@ -88,6 +89,11 @@ public abstract class BaseTest {
     public static String SYSTEM_USER_FOR_FUNCTIONAL_TEST_ORCHESTRATION =
         "hrs.functional.system.user@hmcts.net";
 
+    public static List<String>
+        SYSTEM_USER_FOR_FUNCTIONAL_TEST_ORCHESTRATION_ROLES =
+        List.of("caseworker", "caseworker-hrs", "caseworker-hrs-searcher", "ccd-import", "caseworker-hrs-systemupdate");
+
+
     protected static final String USER_WITH_SEARCHER_ROLE__CASEWORKER_HRS = "em-test-searcher@test.hmcts.net";
     protected static final String USER_WITH_REQUESTOR_ROLE__CASEWORKER_ONLY = "em-test-requestor@test.hmcts.net";
     protected static final String USER_WITH_NONACCESS_ROLE__CITIZEN = "em-test-citizen@test.hmcts.net";
@@ -102,10 +108,15 @@ public abstract class BaseTest {
     public static final String CASEREF_PREFIX = "FUNCTEST_";
 
     public static final String VH_CASEREF_PREFIX = "VH-FUNCT-";
+    protected static List<String> CASE_WORKER_ROLE = List.of("caseworker");
+    protected static List<String> CASE_WORKER_HRS_SEARCHER_ROLE =
+        List.of("caseworker", "caseworker-hrs", "caseworker-hrs-searcher");
+    protected static List<String> CITIZEN_ROLE = List.of("citizen");
     protected static final String CLOSE_CASE = "closeCase";
 
     protected static int FIND_CASE_TIMEOUT = 30;
 
+    static int createUsersBaseTestRunCount = 0;
     protected String hrsS2sAuth;
 
     @Value("${close-ccd-test-cases}")
@@ -131,6 +142,9 @@ public abstract class BaseTest {
     @Value("${idam.hrs-ingestor.password}")
     private String idamHrsIngestorPassword;
 
+    @Value("${upload-cdd-definition}")
+    protected boolean uploadCcdDefinition;
+
     @Autowired
     protected IdamClient idamClient;
 
@@ -151,6 +165,38 @@ public abstract class BaseTest {
 
     @PostConstruct
     public void init() {
+        int maxRuns = 1;
+
+        if (uploadCcdDefinition && createUsersBaseTestRunCount < maxRuns) {
+
+            LOGGER.info("BASE TEST POST CONSTRUCT INITIALISATIONS....");
+            SerenityRest.useRelaxedHTTPSValidation();
+
+
+            LOGGER.info("CREATING HRS FUNCTIONAL TEST SYSTEM USER");
+            createIdamUserIfNotExists(
+                SYSTEM_USER_FOR_FUNCTIONAL_TEST_ORCHESTRATION,
+                SYSTEM_USER_FOR_FUNCTIONAL_TEST_ORCHESTRATION_ROLES
+            );
+
+            LOGGER.info("CREATING REGULAR TEST USERS");
+
+            createIdamUserIfNotExists(USER_WITH_SEARCHER_ROLE__CASEWORKER_HRS, CASE_WORKER_HRS_SEARCHER_ROLE);
+            createIdamUserIfNotExists(USER_WITH_REQUESTOR_ROLE__CASEWORKER_ONLY, CASE_WORKER_ROLE);
+            createIdamUserIfNotExists(USER_WITH_NONACCESS_ROLE__CITIZEN, CITIZEN_ROLE);
+
+            LOGGER.info("IMPORTING CCD DEFINITION");
+
+            try {
+                extendedCcdHelper.importDefinitionFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                LOGGER.error("IMPORTING CCD DEFINITION failed", e);
+            }
+
+            createUsersBaseTestRunCount++;
+
+        }
         LOGGER.info("AUTHENTICATING TEST USER FOR CCD CALLS");
         hrsS2sAuth = BEARER + s2sHelper.getS2sToken();
     }
