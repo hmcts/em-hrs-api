f350e27969ba59498f0e80f28a7f622cc6ce6aa8
mdayican <mdayican@gmail.com>
2025-07-15 14:22:42 +0100

    refactor

--- src/contractTest/java/uk/gov/hmcts/reform/em/hrs/consumer/HearingRecordingDownloadShareeConsumerPactTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/hrs/consumer/HearingRecordingDownloadShareeConsumerPactTest.java
@@ -53,75 +53,66 @@ public class HearingRecordingDownloadShareeConsumerPactTest extends BaseConsumer
         "serviceauthorization", "Bearer some-service-token"
     );
 
-    @Pact(provider = PROVIDER, consumer = CONSUMER)
-    public V4Pact downloadSegmentBySegmentNoPact(PactDslWithProvider builder) {
+    private void testDownload(MockServer mockServer, String reqUrl) {
+        Response response = SerenityRest
+            .given()
+            .headers(headers)
+            .get(mockServer.getUrl() + reqUrl);
+
+        response.then()
+            .statusCode(HttpStatus.OK.value())
+            .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+
+        assertThat(response.asByteArray()).hasSize(8);
+        assertThat(response.getHeader("Content-Disposition"))
+            .isEqualTo("attachment; filename=mocked-file.mp3");
+    }
+
+    public V4Pact buildPact(PactDslWithProvider builder, String path, String given) {
         return builder
-            .given("A segment exists for sharee to download by recording ID and segment number")
+            .given(given)
             .uponReceiving("A request to download a segment for a hearing recording")
-            .path(SEGMENT_DOWNLOAD_PATH_BY_SEGMENT_NO)
+            .path(path)
             .method("GET")
             .headers(headers)
             .willRespondWith()
             .status(HttpStatus.OK.value())
             .headers(
                 Map.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE,
-                       HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=mocked-file.mp3")
+                       HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=mocked-file.mp3"
+                )
             )
             .withBinaryData(CONTENT, "application/octet-stream")
             .toPact(V4Pact.class);
+    }
+
+    @Pact(provider = PROVIDER, consumer = CONSUMER)
+    public V4Pact downloadSegmentBySegmentNoPact(PactDslWithProvider builder) {
+        return buildPact(
+            builder,
+            "A segment exists for sharee to download by recording ID and segment number",
+            SEGMENT_DOWNLOAD_PATH_BY_SEGMENT_NO
+        );
     }
 
     @Test
     @PactTestFor(pactMethod = "downloadSegmentBySegmentNoPact", providerName = PROVIDER)
     void testDownloadSegmentBySegmentNo(MockServer mockServer) {
-        Response response = SerenityRest
-            .given()
-            .headers(headers)
-            .get(mockServer.getUrl() + SEGMENT_DOWNLOAD_PATH_BY_SEGMENT_NO);
-
-        response.then()
-            .statusCode(HttpStatus.OK.value())
-            .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-
-        assertThat(response.asByteArray()).hasSize(8);
-        assertThat(response.getHeader("Content-Disposition"))
-            .isEqualTo("attachment; filename=mocked-file.mp3");
+        testDownload(mockServer, SEGMENT_DOWNLOAD_PATH_BY_SEGMENT_NO);
     }
-
 
     @Pact(provider = PROVIDER, consumer = CONSUMER)
     public V4Pact downloadSegmentByFileNameNoPact(PactDslWithProvider builder) {
-        return builder
-            .given("A segment exists for sharee to download by recording ID and file name")
-            .uponReceiving("A request to download a segment for a hearing recording by file name")
-            .path(SEGMENT_DOWNLOAD_PATH_BY_FILE_NAME)
-            .method("GET")
-            .headers(headers)
-            .willRespondWith()
-            .status(HttpStatus.OK.value())
-            .headers(
-                Map.of(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE,
-                       HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=mocked-file.mp3")
-            )
-            .withBinaryData(CONTENT, "application/octet-stream")
-            .toPact(V4Pact.class);
+        return buildPact(
+            builder,
+            "A segment exists for sharee to download by recording ID and file name",
+            SEGMENT_DOWNLOAD_PATH_BY_FILE_NAME
+        );
     }
 
     @Test
     @PactTestFor(pactMethod = "downloadSegmentByFileNameNoPact", providerName = PROVIDER)
     void testDownloadSegmentByFileNameNo(MockServer mockServer) {
-        Response response = SerenityRest
-            .given()
-            .headers(headers)
-            .get(mockServer.getUrl() + SEGMENT_DOWNLOAD_PATH_BY_FILE_NAME);
-
-        response.then()
-            .statusCode(HttpStatus.OK.value())
-            .contentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-
-        assertThat(response.asByteArray()).hasSize(8);
-        assertThat(response.getHeader("Content-Disposition"))
-            .isEqualTo("attachment; filename=mocked-file.mp3");
+        testDownload(mockServer, SEGMENT_DOWNLOAD_PATH_BY_FILE_NAME);
     }
-
 }
