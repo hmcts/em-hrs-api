047f0d26f08a2116a0072588321ef0d970fb085b
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-11-18 11:08:02 +0000

    rename class

--- src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/config/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/config/OpenAPIPublisherTest.java
@@ -20,7 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @WebMvcTest
 @AutoConfigureMockMvc
-class SwaggerPublisherTest {
+class OpenAPIPublisherTest {
 
     @Autowired
     private MockMvc mvc;
