97bf5a9e032f75f660d3970f7aaf446d81dcc8a5
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-11-18 15:32:53 +0000

    update path

--- src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/config/OpenAPIPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/em/hrs/config/OpenAPIPublisherTest.java
@@ -86,7 +86,7 @@ class OpenAPIPublisherTest {
             .getResponse()
             .getContentAsByteArray();
 
-        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/swagger-specs.json"))) {
+        try (OutputStream outputStream = Files.newOutputStream(Paths.get("/tmp/openapi-specs.json"))) {
             outputStream.write(specs);
         }
     }
