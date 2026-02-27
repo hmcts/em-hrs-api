775e9650c2fae76faa6f6351446b793443c0ac28
michaeljacob <Michael.Jacob@HMCTS.NET>
2026-02-27 09:09:30 +0000

    add iterations

--- src/main/java/uk/gov/hmcts/reform/em/hrs/job/UpdateTtlTask.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/job/UpdateTtlTask.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.em.hrs.job;
 
-import com.google.common.collect.Lists;
 import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,11 +30,11 @@ public class UpdateTtlTask implements Runnable {
     @Value("${scheduling.task.update-ttl.batch-size}")
     private int batchSize;
 
+    @Value("${scheduling.task.update-ttl.no-of-iterations}")
+    private int noOfIterations;
+
     @Value("${scheduling.task.update-ttl.thread-limit}")
     private int defaultThreadLimit;
-
-    @Value("${scheduling.task.update-ttl.max-records}")
-    private int maxRecordsToProcess;
 
     public UpdateTtlTask(HearingRecordingService hearingRecordingService,
                          CcdDataStoreApiClient ccdDataStoreApiClient) {
@@ -49,42 +48,52 @@ public class UpdateTtlTask implements Runnable {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
-        List<HearingRecordingTtlMigrationDTO> recordsToUpdate =
-            hearingRecordingService.getRecordingsForTtlUpdate(maxRecordsToProcess);
-
-        if (recordsToUpdate.isEmpty()) {
-            logger.info("No records found requiring TTL update.");
-            return;
-        }
-
-        logger.info("Found {} records to process", recordsToUpdate.size());
-
-        List<List<HearingRecordingTtlMigrationDTO>> batches = Lists.partition(recordsToUpdate, batchSize);
-
-        // Cache TTL per CCD case id. Ensures CCD is updated once per case id and HRS is updated for every record.
         Map<Long, LocalDate> ttlByCcdCaseId = new ConcurrentHashMap<>();
 
-        try (ExecutorService executorService = Executors.newFixedThreadPool(defaultThreadLimit)) {
-            for (List<HearingRecordingTtlMigrationDTO> batch : batches) {
-                executorService.submit(() -> processBatch(batch, ttlByCcdCaseId));
+        for (int i = 0; i < noOfIterations; i++) {
+            StopWatch iterationStopWatch = new StopWatch();
+            iterationStopWatch.start();
+
+            StopWatch hrsGetQueryStopWatch = new StopWatch();
+            hrsGetQueryStopWatch.start();
+
+            List<HearingRecordingTtlMigrationDTO> recordsToUpdate =
+                hearingRecordingService.getRecordingsForTtlUpdate(batchSize);
+
+            hrsGetQueryStopWatch.stop();
+            logger.info("Time taken to get {} rows from DB : {} ms", recordsToUpdate.size(),
+                        hrsGetQueryStopWatch.getDuration().toMillis());
+
+            if (recordsToUpdate.isEmpty()) {
+                iterationStopWatch.stop();
+                logger.info(
+                    "No records found requiring TTL update. Time taken to complete iteration number : {} was : {} ms",
+                    i, iterationStopWatch.getDuration().toMillis()
+                );
+                break;
             }
-        } catch (Exception e) {
-            logger.error("Error in executor service during {} task", TASK_NAME, e);
+
+            try (ExecutorService executorService = Executors.newFixedThreadPool(defaultThreadLimit)) {
+                for (HearingRecordingTtlMigrationDTO recording : recordsToUpdate) {
+                    executorService.submit(() -> {
+                        try {
+                            processRecording(recording, ttlByCcdCaseId);
+                        } catch (Exception e) {
+                            logger.error("Failed to process recording ID: {}", recording.id(), e);
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                logger.error("Error in executor service during {} task", TASK_NAME, e);
+            }
+
+            iterationStopWatch.stop();
+            logger.info("Time taken to complete iteration number : {} was : {} ms", i,
+                        iterationStopWatch.getDuration().toMillis());
         }
 
         stopWatch.stop();
         logger.info("Finished {} job. Took {} ms", TASK_NAME, stopWatch.getDuration().toMillis());
-    }
-
-    private void processBatch(List<HearingRecordingTtlMigrationDTO> batch,
-                              Map<Long, LocalDate> ttlByCcdCaseId) {
-        for (HearingRecordingTtlMigrationDTO recording : batch) {
-            try {
-                processRecording(recording, ttlByCcdCaseId);
-            } catch (Exception e) {
-                logger.error("Failed to process recording ID: {}", recording.id(), e);
-            }
-        }
     }
 
     private void processRecording(HearingRecordingTtlMigrationDTO recording,
