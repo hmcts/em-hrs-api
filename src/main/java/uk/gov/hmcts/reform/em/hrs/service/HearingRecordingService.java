fe363f04c8f00149a7ef413db5de4819d13438a0
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2026-03-24 16:46:11 +0000

    Removed UpdateTtlTask and related lines of code. (#2954)

--- src/main/java/uk/gov/hmcts/reform/em/hrs/service/HearingRecordingService.java
+++ src/main/java/uk/gov/hmcts/reform/em/hrs/service/HearingRecordingService.java
@@ -2,13 +2,9 @@ package uk.gov.hmcts.reform.em.hrs.service;
 
 import uk.gov.hmcts.reform.em.hrs.domain.HearingRecording;
 import uk.gov.hmcts.reform.em.hrs.dto.HearingRecordingDto;
-import uk.gov.hmcts.reform.em.hrs.dto.HearingRecordingTtlMigrationDTO;
 
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public interface HearingRecordingService {
 
@@ -21,8 +17,4 @@ public interface HearingRecordingService {
     HearingRecording createHearingRecording(HearingRecordingDto recordingDto);
 
     HearingRecording updateCcdCaseId(HearingRecording recording, Long ccdCaseId);
-
-    List<HearingRecordingTtlMigrationDTO> getRecordingsForTtlUpdate(int limit);
-
-    void updateTtl(UUID recordingId, LocalDate ttl);
 }
